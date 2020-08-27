0dbbe3416224efe7307450558f11a7b8e0e658e8
daejo <daejo@icloud.com>
2020-08-27 15:09:28 -0700

    adds temp conversion

--- assets/script.js
+++ assets/script.js
@@ -25,10 +25,26 @@
 var cityInputEl = document.querySelector ("#city-search");
 var citySearchTerm = document.querySelector("#city-search-term");
 var cityFormEl = document.querySelector("#city-form");
-var apiUrl = ;
-var apiKey = "cd7fcf2b24666d2644afde8dd6cfcd12";
+var apiUrl = "pro.openweathermap.org/data/2.5/forecast/hourly?"; // Open Weather API URL
+var apiKey = "cd7fcf2b24666d2644afde8dd6cfcd12"; // My Openweather API key.
 
 
+function convertCtoF (temperature) {
+    (temperature * 9/5) + 32;
+};
+
+tempEl.addEventListener("click" function() { // Convert C to F and F to C with click.
+    if(weather.temperatue.value === undefined) return;
+    if(weather.temperature.unit === "celsius") {
+        var fahrenheit = convertCtoF(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        tempEl.innerHTML = "${fahrenheit} <span>F</span>";
+        weather.temperature.unit = "fahrenheit";
+    } else {
+        tempEl.innerHTML = "${weather.temperature.value} <span>C</span>";
+        weather.temperature.unit = "celsius";
+    }
+});
 
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
