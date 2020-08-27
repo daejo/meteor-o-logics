21b05dc9c2a4efcc67eb417fa5aa615499070f45
daejo <daejo@icloud.com>
2020-08-27 13:12:27 -0700

    adds psuedocode

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+/*  CREATE A WEATHER APP
+    
+    * Search for a city's weather conditions
+        - Create framework and flow.
+        - Add form for text to be inputed in.
+        - Add search function to look for cities.
+        - Present current and future conditions for that city and that city is added to the search history.
+    
+    * Display current weather conditions for that city
+        - Display name of city searched.
+        - Display date
+        - Display an icon for weather condition representation.
+        - Display temperature, humidity, wind speed, and UV index.
+
+    * UV index and future weather conditions
+        - Display color that indicates weather conditions as favorable, moderate, or severe.
+        - Present a 5-day forecast that displays date, icon of weather conditions, temperature, and humidity.
+
+    * Search history search
+        - Use stored local data city names
+        - Display the current and future conditions upon clicking a city name in the search history.
+*/
